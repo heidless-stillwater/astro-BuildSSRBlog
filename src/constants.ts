32a01af74419028419cee33eaee1130062778303
heidless <lockhart.r@gmail.com>
2025-02-03 13:04:24 +0000

    pagination complete

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 export const SITE_TITLE = "pdfNinja";
 export const SITE_DESCRIPTION = "pdf utilities & tool for daily use";
 export const HOMEPAGE_ARTICLE_LIMIT = 6;
-export const ARTICLES_PER_PAGE = 6;
+export const ARTICLES_PER_PAGE6= 3;
 
 
